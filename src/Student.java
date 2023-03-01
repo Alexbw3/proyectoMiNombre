3ddb23883daa19c263e676f78078567e44b698b4
alex lira barrios <22670046@itiguala.edu.mx>
2023-03-01 07:50:16 -0600

    proyectoMiNombree

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Student {
+public class Student {
+    private String name;
+    private String lastname;
+    private long controlNumber;
+    private String career;
+
+
+    public String getName(){
+        return this.name;
+    }
+    public String getLastname(){
+        return this.lastname;
+    }
+
+    public String getCareer() {
+        return career;
+    }
+
+    public long getControlNumber() {
+        return controlNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setControlNumber(long controlNumber) {
+        this.controlNumber = controlNumber;
+    }
+
+    public void setCareer(String career) {
+        this.career = career;
+    }
 }
--- src/TestStudent.java
+++ src/TestStudent.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class TestStudent {
+public class TestStudent {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("ricardo");
+        student.setLastname("lira");
+        student.setControlNumber(22670046);
+        student.setCareer("ing.informatica");
+        System.out.println("nombre:" + student.getName());
+        System.out.println("apellido:" + student.getLastname());
+        System.out.println("numero de control"+student.getControlNumber());
+        System.out.println("carrera:"+student.getCareer());
+
+        Student ricardo = new Student();
+        ricardo.setName("Evelyn");
+        ricardo.setLastname("lira");
+        ricardo.setControlNumber(22670067);
+        ricardo.setCareer("ing.informatica");
+        System.out.println("nombre:" + ricardo.getName());
+        System.out.println("apellido:" + ricardo.getLastname());
+        System.out.println("numero de control" +ricardo.getControlNumber());
+        System.out.println("carrera:"+ricardo.getCareer());
+
+
+    }
 }
