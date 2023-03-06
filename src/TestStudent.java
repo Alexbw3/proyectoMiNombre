c0ae236348b082328c14197691194e81c50e087a
Alexbw3 <124899155+Alexbw3@users.noreply.github.com>
2023-03-05 21:05:08 -0600

    Update TestStudent.java

--- src/TestStudent.java
+++ src/TestStudent.java
@@ -5,6 +5,7 @@ public class TestStudent {
         student.setLastname("lira");
         student.setControlNumber(22670076);
         student.setCareer("ing.informatica");
+        student.toString();
         System.out.println("nombre:" + student.getName());
         System.out.println("apellido:" + student.getLastname());
         System.out.println("numero de control"+student.getControlNumber());
@@ -15,6 +16,7 @@ public class TestStudent {
         ricardo.setLastname("Barrios");
         ricardo.setControlNumber(22670046);
         ricardo.setCareer("ing.informatica");
+        student.toString();
         System.out.println("nombre:" + ricardo.getName());
         System.out.println("apellido:" + ricardo.getLastname());
         System.out.println("numero de control" +ricardo.getControlNumber());
